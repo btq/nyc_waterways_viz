dc2c808e3cf252210134a07cb5d8661495aa6898
Brian Quinn <btquinn@gmail.com>
2016-02-21 14:54:43 -0800

    Parse main page, next parse xml

--- python/parse_noaa_links.py
+++ python/parse_noaa_links.py
@@ -6,7 +6,8 @@ Created on Wed Feb 17 15:34:44 2016
 """
 import requests
 from bs4 import BeautifulSoup
-
+LON = [-74.0367, -73.9026]
+LAT = [40.6823, 40.8840]
 state_pages = ['http://tidesandcurrents.noaa.gov/noaacurrents/Stations?g=458','http://tidesandcurrents.noaa.gov/noaacurrents/Stations?g=457']
 req = requests.get(state_pages[0])
 soup = BeautifulSoup(req.text)
@@ -14,9 +15,11 @@ rows = soup.find_all('tr')
 for r in rows:
     if r.a:
         link = r.a['href']
-        coords = r.a['onmouseover'].split(',')[1:]
-        []
+        latlon = [float(c.strip(' );')) for c in r.a['onmouseover'].split(',')[1:]]
+        latlon[1] *= -1        
+        if latlon[0] > LAT[0] and latlon[0] < LAT[1] and latlon[1] > LON[0] and latlon[1] < LON[1]:
+            print latlon, r.a.text, link
+            
 
-parties = partylist.find_all('div',attrs={'class': 'views-row'})
 #for sp in state_pages:
 #    req = requests.get(sp)
